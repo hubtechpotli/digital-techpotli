98899ceba1c51de9d18246388cbbe67da3af31d3
Ravijha11 <ravijha110903@gmail.com>
2025-12-22 11:34:51 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@ import Footer from "@/components/custom/Footer";
 import Navbar from "@/components/custom/Navbar";
 import { WhatsAppChatbot } from "@/components/custom/WhatsAppChatbot";
 import { FirstVisitBanner } from "@/components/custom/FirstVisitBanner";
-import { GTM } from "@/components/custom/GTM";
 import { SimpleAuthProvider } from "@/lib/auth/SimpleAuthProvider";
 import { defaultMetadata } from "@/lib/metadata";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -42,6 +42,49 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager - Server-side rendered for page source visibility */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K79WJLLQ');`,
+          }}
+        />
+        {/* Google Analytics (GA4) - Server-side rendered for page source visibility */}
+        <Script
+          strategy="beforeInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-C1CVPMMB5K"
+        />
+        <Script
+          id="ga4-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C1CVPMMB5K', {
+                page_path: window.location.pathname,
+              });
+              gtag('config', 'G-J7BFN4WEM3', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K79WJLLQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {/* Structured Data - Organization and WebSite Schema (server-side rendered) */}
         <script
           type="application/ld+json"
@@ -70,7 +113,6 @@ export default function RootLayout({
             }),
           }}
         />
-        <GTM />
         <SimpleAuthProvider>
           <div className="min-h-screen w-full">
             <Navbar />
